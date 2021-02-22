20b62f611ac133203f9136822aa78cc09c6f988f
joeoke <joe.okelue7@gmail.com>
2021-02-22 13:20:44 +0000

    added validation checks and interactive messaging

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   });
 
   const [submitted, setSubmitted] = useState(false)
+  const [valid, setValid]= useState(false)
 
   const handleFirstNameInputChange = (event) => {  
     setValues({...values, firstName:event.target.value})  
@@ -25,31 +26,40 @@ function App() {
 
   const handleSubmit = (e) =>{
     e.preventDefault()
+
+    if(values.firstName && values.lastName && values.email){
+      setValid(true)
+    }
+    
     setSubmitted(true)
-  }
+    }    
+  
 
   return (
     <div className="form-container">
       <form onSubmit={handleSubmit} className="register-form">
-        {submitted ? <div className="success-message">Success! Thank you for registering</div> : null}
+        {submitted && valid ? <div className="success-message">Success! Thank you for registering</div> : null}
         <input onChange={handleFirstNameInputChange}
           value={values.firstName}
           className="form-field"
           placeholder="First Name"
           name="firstName"/>
-        {submitted && !values.firstName ? <span>please enter your first name</span>:null}
-        {submitted && !values.firstName ? setSubmitted(false):null}
-
+        {submitted && !values.firstName ? <span>please enter your first name</span> : null}
+        
         <input onChange={handleLastNameInputChange}
           value={values.lastName}
           className="form-field"
           placeholder="Last Name"
           name="lastName"/> 
+        {submitted && !values.lastName ? <span>Please enter your last name</span> : null}
+
         <input onChange={handleEmailInputChange} 
           value={values.email}
           className="form-field"
           placeholder="Email"
           name="email"/> 
+        {submitted && !values.email ? <span>Please enter your email</span> : null}
+
         <button className="form-field" type="submit">Register</button>
       </form>      
     </div>
